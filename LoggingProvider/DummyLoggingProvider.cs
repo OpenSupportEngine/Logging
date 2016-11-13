c7baed5ba0307c7976ece9a1be6fb4a38047f409
321polorex123 <info@321polorex123.com>
2016-11-13 23:27:33 +0100

    Added dummy logging provider

---
+++ LoggingProvider/DummyLoggingProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSupportEngine.Logging.LoggingFormatProvider;
+
+namespace OpenSupportEngine.Logging.LoggingProvider
+{
+    public class DummyLoggingProvider : ILoggingProvider
+    {
+        public ILoggingFormatProvider FormatProvider { get; } =
+            new StandardLoggingFormatProvider();
+
+        public bool IsCrashResistent { get; } = true;
+
+        public void FinishLog()
+        {
+        }
+
+        public void LogMessage(string message, LoggingCategory category = LoggingCategory.Standard)
+        {
+        }
+
+        public void StartLog()
+        {
+        }
+    }
+}
